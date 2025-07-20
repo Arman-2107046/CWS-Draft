8230d3b134c9250196c013358d4dd00d668b12f5
Arman-2107046 <armanr.rafi@gmail.com>
2025-07-20 15:52:03 +0600

    deleted console log

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -18,7 +18,7 @@ const ContactForm = () => {
     };
 
     try {
-      console.log("API URL:", import.meta.env.VITE_API_URL);
+      // console.log("API URL:", import.meta.env.VITE_API_URL);
 
       const response = await fetch(`${import.meta.env.VITE_API_URL}/send-email`, {
         method: "POST",
